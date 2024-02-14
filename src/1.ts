262771e8a3dc4fdb690e383628fe019135693fb3
Serhii Rybak <serhiirybak@Serhiis-MacBook-Pro.local>
2024-02-14 23:20:33 +0100

    Some refactoring was done

--- src/1.ts
+++ src/1.ts
@@ -5,9 +5,7 @@
 */
 
 class Student {
-  constructor(public name: string,public age: number,public grade: string) {
-    
-  }
+  constructor(public name: string, public age: number, public grade: string) {}
 }
 
 export {};
